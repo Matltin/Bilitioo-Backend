e42c1a45dd8a3d1dae8679aa8de2537dffaa4eb3
Matltin <ma1383ahmadi@gmail.com>
2025-05-28 17:14:57 +0330

    ticket getTicket

--- api/server.go
+++ api/server.go
@@ -1,7 +1,9 @@
 package api
 
 import (
+	"database/sql"
 	"log"
+	"net/http"
 
 	db "github.com/Matltin/Bilitioo-Backend/db/sqlc"
 	"github.com/Matltin/Bilitioo-Backend/token"
@@ -23,8 +25,8 @@ func NewServer(config util.Config, db *db.Queries) *Server {
 	}
 
 	ser := &Server{
-		config:  config,
-		Queries: db,
+		config:     config,
+		Queries:    db,
 		tokenMaker: tokenMaker,
 	}
 
@@ -38,7 +40,6 @@ func (ser *Server) setupRouter() {
 
 	router.POST("/sign-in", ser.signUpUser)
 	router.POST("/log-in", ser.logInUser)
-	router.POST("/tickets", ser.searchTickets)
 
 	authRoutes := router.Group("/").Use(authMiddleware(ser.tokenMaker))
 
@@ -47,8 +48,6 @@ func (ser *Server) setupRouter() {
 	authRoutes.GET("/city", ser.getCities)
 	authRoutes.POST("/city", ser.searchTicketsByCities)
 	authRoutes.GET("/ticket-detail/:ticket_id", ser.getTicketDetails)
-	
-	// router.POST("tickets", ser.searchTickets)
 
 	ser.router = router
 }
@@ -59,4 +58,51 @@ func (server *Server) Start(add string) {
 
 func errorResponse(err error) gin.H {
 	return gin.H{"error": err.Error()}
+}
+
+type getTicketDetailsRequest struct {
+	TicketID int64 `uri:"ticket_id" binding:"required"`
+}
+
+func (server *Server) getTicketDetails(ctx *gin.Context) {
+	var req getTicketDetailsRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	ticket, err := server.Queries.GetTicketDetails(ctx, req.TicketID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	response := gin.H{
+		"origin":        ticket.Origin,
+		"destination":   ticket.Destination,
+		"departureTime": ticket.DepartureTime,
+		"arrivalTime":   ticket.ArrivalTime,
+		"amount":        ticket.Amount,
+		"capacity":      ticket.Capacity,
+		"vehicle_type":  ticket.VehicleType,
+		"feature":       ticket.Feature,
+	}
+
+	switch ticket.VehicleType {
+	case db.VehicleTypeBUS:
+		response["VIP"] = ticket.VIP.Bool
+		response["bed_chair"] = ticket.BedChair.Bool
+	case db.VehicleTypeTRAIN:
+		response["have_compartment"] = ticket.HaveCompartment.Bool
+		response["rank"] = ticket.Rank.Int32
+	case db.VehicleTypeAIRPLANE:
+		response["flight_class"] = string(ticket.FlightClass.FlightClass)
+		response["airplane_name"] = ticket.AirplaneName.String
+	}
+
+	ctx.JSON(http.StatusOK, response)
 }
