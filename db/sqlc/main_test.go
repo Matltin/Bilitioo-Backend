8ae5beabbfd4166ad7a6c7362d750024ca27e248
Matltin <ma1383ahmadi@gmail.com>
2025-07-04 11:18:30 +0330

    add main_test.go file

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	testDB, err := sql.Open("postgres", "postgresql://root:secret@localhost:5432/bilitioo?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	testQueries = New(testDB)
+
+	os.Exit(m.Run())
+}
