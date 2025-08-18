61e134ea74dba1fe758cd9bede39607179f086f8
Matltin <ma1383ahmadi@gmail.com>
2025-08-18 19:56:47 +0330

    remove temprory hash function

--- util/password.go
+++ util/password.go
@@ -1,49 +1,42 @@
 package util
 
-import (
-	"crypto/rand"
-	"crypto/sha256"
-	"encoding/hex"
-	"errors"
-	"fmt"
-	"strings"
-)
-
 // HashedPassword generates a SHA-256 hash of the password with a random salt
 func HashedPassword(password string) (string, error) {
 	// Generate a random salt
-	salt := make([]byte, 16)
-	if _, err := rand.Read(salt); err != nil {
-		return "", fmt.Errorf("failed to generate salt: %w", err)
-	}
+	// salt := make([]byte, 16)
+	// if _, err := rand.Read(salt); err != nil {
+	// 	return "", fmt.Errorf("failed to generate salt: %w", err)
+	// }
 
-	// Create SHA-256 hash of password + salt
-	hash := sha256.Sum256([]byte(password + string(salt)))
+	// // Create SHA-256 hash of password + salt
+	// hash := sha256.Sum256([]byte(password + string(salt)))
 
-	// Return format: salt:hash
-	return fmt.Sprintf("%x:%x", salt, hash), nil
+	// // Return format: salt:hash
+	// return fmt.Sprintf("%x:%x", salt, hash), nil
+
+	return password, nil
 }
 
 // CheckPassword verifies if the provided password matches the hashed password
 func CheckPassword(password string, hashedPassword string) error {
 	// Split salt and hash
-	parts := strings.Split(hashedPassword, ":")
-	if len(parts) != 2 {
-		return errors.New("invalid hash format")
-	}
+	// parts := strings.Split(hashedPassword, ":")
+	// if len(parts) != 2 {
+	// 	return errors.New("invalid hash format")
+	// }
 
-	salt, err := hex.DecodeString(parts[0])
-	if err != nil {
-		return fmt.Errorf("failed to decode salt: %w", err)
-	}
+	// salt, err := hex.DecodeString(parts[0])
+	// if err != nil {
+	// 	return fmt.Errorf("failed to decode salt: %w", err)
+	// }
 
-	// Compute hash of provided password with the same salt
-	hash := sha256.Sum256([]byte(password + string(salt)))
+	// // Compute hash of provided password with the same salt
+	// hash := sha256.Sum256([]byte(password + string(salt)))
 
-	// Compare with stored hash
-	if hex.EncodeToString(hash[:]) != parts[1] {
-		return errors.New("passwords do not match")
-	}
+	// // Compare with stored hash
+	// if hex.EncodeToString(hash[:]) != parts[1] {
+	// 	return errors.New("passwords do not match")
+	// }
 
 	return nil
 }
