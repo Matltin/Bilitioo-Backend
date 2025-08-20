4a222a709a0f244e248cdcb91f97533d5528f3ff
ryhn-mir <Rmirarabshahi2005@gmail.com>
2025-08-20 20:14:06 +0330

    fix gonfig.go

--- util/config.go
+++ util/config.go
@@ -9,16 +9,17 @@ import (
 // Config fstores all configuration of the application.
 // The values are read by viper from a config file or enviroment variables.
 type Config struct {
-	ENVIROMENT          string        `mapstructure:"ENVIROMENT"`
-	DBDriver            string        `mapstructure:"DB_DRIVER"`
-	DBSource            string        `mapstructure:"DB_SOURCE"`
-	HTTPServerAddress   string        `mapstructure:"HTTP_SERVER_ADDRESS"`
-	TokenSymmetrickey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
-	RedisAddress        string        `mapstructure:"REDIS_ADDRESS"`
-	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
-	EmailSenderName     string        `mapstructure:"EMAIL_SENDER_NAME"`
-	EmailSenderAdderss  string        `mapstructure:"EMAIL_SENDER_ADDRESS"`
-	EmailSenderPassword string        `mapstructure:"EMAIL_SENDER_PASSWORD"`
+	ENVIROMENT           string        `mapstructure:"ENVIROMENT"`
+	DBDriver             string        `mapstructure:"DB_DRIVER"`
+	DBSource             string        `mapstructure:"DB_SOURCE"`
+	HTTPServerAddress    string        `mapstructure:"HTTP_SERVER_ADDRESS"`
+	TokenSymmetrickey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	ElasticsearchAddress string        `mapstructure:"ELASTICSEARCH_ADDRESS"`
+	RedisAddress         string        `mapstructure:"REDIS_ADDRESS"`
+	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+	EmailSenderName      string        `mapstructure:"EMAIL_SENDER_NAME"`
+	EmailSenderAdderss   string        `mapstructure:"EMAIL_SENDER_ADDRESS"`
+	EmailSenderPassword  string        `mapstructure:"EMAIL_SENDER_PASSWORD"`
 }
 
 // LoadConfig reads configuration from file or envirement variables.
